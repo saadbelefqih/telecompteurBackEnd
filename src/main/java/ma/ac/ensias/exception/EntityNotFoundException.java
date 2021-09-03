e201edfee1d84c2835944f5d93f0f8320fa8540c
saadbelefqih <saad.belefqih@gmail.com>
2021-09-03 01:16:04 +0100

    update api customer

--- src/main/java/ma/ac/ensias/exception/EntityNotFoundException.java
+++ src/main/java/ma/ac/ensias/exception/EntityNotFoundException.java
@@ -2,9 +2,7 @@ package ma.ac.ensias.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	public EntityNotFoundException(String message ) {
 		super(message);
